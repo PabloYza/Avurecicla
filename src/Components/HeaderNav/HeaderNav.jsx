d0d989bfb295506ac7b7e46227ed3d3ddb4d69c0
Pablo <p.deyzaguirre13@gmail.com>
2022-02-21 18:49:04 +0100

    restored /public folder

--- src/Components/HeaderNav/HeaderNav.jsx
+++ src/Components/HeaderNav/HeaderNav.jsx
@@ -25,7 +25,7 @@ const HeaderNav = () => {
                 <Link to="/Beneficios">Beneficios</Link>
               </div>
               <div>
-                <Link to="/CalculateFootPrint">Calcula tu Huella</Link>
+                <Link to="/CalculateFootPrint">Calcula tu uella</Link>
               </div>
             </div>
           </div>
